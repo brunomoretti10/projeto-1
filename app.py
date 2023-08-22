7968cd888c31e34c72f39f86c5c52d8892fc12bf
brunomoretti10 <brunomoretti1408@gmail.com>
2023-08-21 22:40:58 -0300

    primeiro commit phyton

---
+++ app.py
@@ -0,0 +1,15 @@
+
+#  modificando o tipo de dados
+x = str(8)
+y = int(10)
+z = float(5)
+print(x+x)
+print(y+y)
+print(z)
+
+# Bruno tem 20 anos de idade e mora na cidade de São Paulo
+nome = 'Bruno'
+idade = str(20)
+cidade = 'Sao Paulo'
+
+print(nome + ' tem ' + idade + ' anos de idade e mora na cidade de ' + cidade)
