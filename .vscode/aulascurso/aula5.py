d1fafb99a1c7c5cff7c5b5ba94c76f33dd106a09
brunomoretti10 <brunomoretti1408@gmail.com>
2023-08-22 17:05:02 -0300

    aula 5: operadores matemáticos

---
+++ .vscode/aulascurso/aula5.py
@@ -0,0 +1,58 @@
+#operadores matemáticos
+'''
+adição +
+subtração -
+multiplicação *
+divisão /
+potenciação **
+'''
+print(2+2)
+print(2-1)
+print(2*3)
+print(2/2)
+print(2**4)
+
+#operadores comparativos
+'''
+igual==
+diferente !=
+maior que >
+menor que <
+maior ou igual >=
+menor ou igual <=
+'''
+print(10==10)
+print(10!=8)
+print(10>12)
+print(10<8)
+print(10>=10)
+print(10<=11)
+
+#operadores de atribuição
+'''
++= acrescenta 
+-= subtrai
+*= multiplica 
+/= divide
+**= potencia
+'''
+x=5
+x+=5
+print(x)
+y=3
+y-=2
+print(y)
+z=10
+z*=2
+print(z)
+l=8
+l/=2
+print(l)
+b=2
+b**=3
+print(b)
+
+# %= o caracter % significa o 'resto', o que sobra de uma divisão
+s=50
+s%=7
+print(s) # 7*7 = 49, sobrando 1 para 50, logo o output será 1
