b1f60a0d6a20a5f1ad880bbbabf99a2f3c5a7c32
brunomoretti10 <brunomoretti1408@gmail.com>
2023-08-22 14:16:54 -0300

    aula 3: slice, index

---
+++ .vscode/aulascurso/aula3.py
@@ -0,0 +1,13 @@
+# slice = fatiar
+clube = 'barcelona'
+# index  012345678
+print(clube[0])  # b
+
+print(clube[0:3]) # bar #phyton pega um caracter antes, se fosse 0:2, o output viria 'ba'
+
+print(clube[-1]) # phyton o valor negativo funciona, pega de trás para a frente
+
+valor = str(99.75)  # imprimir só os centavos #converter em str
+# index     01234
+print(valor[3:5])
+print(valor[2:])  # caso não escreva nada, puxará o final
