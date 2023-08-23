e45caac823861aeb0a25d300520ddf45214be6db
brunomoretti10 <brunomoretti1408@gmail.com>
2023-08-23 15:02:48 -0300

    aula 9: for loop em strings

---
+++ .vscode/controle de fluxo, declarações condicionadas/aula9.py
@@ -0,0 +1,22 @@
+# for loop - utilizando strings
+# Primeiro loop para imprimir 'google'
+for letra in 'google':
+    print(letra)
+
+# Segundo loop para imprimir 'spfc'
+palavra = 'spfc'
+for caracteres in palavra:
+    print(caracteres)
+
+# Terceiro loop para imprimir '10'
+for num in '10':
+    print(num)
+
+# quarto loop
+for letras in 'spfc':
+    print(letras + ' esta dentro da palavra spfc')
+
+# quinto loop - formated string
+    palavras = 'bruno'
+for letra1 in palavras:
+    print(f'{letra1} esta dentro da palavra {palavras}')
