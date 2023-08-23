7debe6c41faceb2918daf74b3b7f159f9e139607
brunomoretti10 <brunomoretti1408@gmail.com>
2023-08-23 16:09:24 -0300

    aula 12: for loop separando strings usando end=''

---
+++ .vscode/controle de fluxo, declarações condicionadas/aula12.py
@@ -0,0 +1,10 @@
+# for loop - separando strings
+# modificar fantastico para f a n t a s t i c o
+# ao definir end='', você pode fazer com que a próxima impressão continue na mesma linha
+
+palavra = 'FANTASTICO'
+for space in palavra:
+    print(f' {space}', end='')
+    
+  #  print('olá', end=' ')
+   # print('bom dia') output: olá, bom dia
