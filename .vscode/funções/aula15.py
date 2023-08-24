eafc983c80faa7f8eec195d3a17e770d264956ce
brunomoretti10 <brunomoretti1408@gmail.com>
2023-08-23 23:55:58 -0300

    aula 15: funções de soma

---
+++ .vscode/funções/aula15.py
@@ -0,0 +1,22 @@
+# funções
+# def - antes da função
+
+def boas_vindas():
+    print('olá bruno!')
+    print('tudo bem?')
+    # para imprimir, temos que chamar a função
+
+
+boas_vindas()
+
+# criando uma função de soma
+
+
+def soma_dois_numeros():
+    numero1 = 10
+    numero2 = 11
+    resultado = numero1+numero2
+    print(resultado)
+
+
+soma_dois_numeros()
