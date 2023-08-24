3e2fcd2de64c5cfc5068754d86dceb7471d00b26
brunomoretti10 <brunomoretti1408@gmail.com>
2023-08-24 14:53:47 -0300

    aula 18: import, módulos, funções já prontas

---
+++ .vscode/funções/aula18.py
@@ -0,0 +1,13 @@
+# procurar no google as biblioteca/módulos do que você quer em phyton
+import math
+print(math.factorial(4))  # mais fácil
+# importando um módulo
+# qual o numero fatorial de 4?
+# 4*3*2*1
+
+fatorial = 4*3*2*1
+print(fatorial)
+
+print(math.factorial(5))
+print(math.floor(9.8))
+print(math.ceil(9.8))
