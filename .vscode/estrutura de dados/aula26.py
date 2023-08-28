43ad28888d5b1158746e18ff357848ceb49f515e
brunomoretti10 <brunomoretti1408@gmail.com>
2023-08-27 21:54:43 -0300

    aula 26: lista dentro de dicionário

---
+++ .vscode/estrutura de dados/aula26.py
@@ -0,0 +1,12 @@
+# itens, keys e values
+aluno = {'Nome': ' Bruno',
+         'idade': 20,
+         'nota': '10',
+         'aprovação': True,
+         'materias': ['física', 'matemática', 'Modelagem de dados']} # lista dentro de dicionário
+print(aluno)
+print(aluno.get('materias'))
+print(aluno.keys())
+print(aluno.values())
+print(aluno.items())
+print(len(aluno))
