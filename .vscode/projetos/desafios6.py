358b172a5c8136a3c7f71dc60c5f72d1877c800c
brunomoretti10 <brunomoretti1408@gmail.com>
2023-08-31 03:33:54 -0300

    desafio lambda

---
+++ .vscode/projetos/desafios6.py
@@ -0,0 +1,22 @@
+# def cubo(num):
+#   return num**3
+
+# transformar em uma única linha de código usando a função lambda
+
+'''cubo = lambda num: num**3
+
+
+user_namber = int(input('digite um número:'))
+print(f'O cubo de {user_namber} é {cubo(user_namber)}')
+'''
+
+# lambda multiplicando
+
+
+def multi(num1, num2): return num1 * num2
+
+
+user_number1 = int(input('digite um número:'))
+user_number2 = int(input('digite mais um número:'))
+print(
+    f'A multiplicação de {user_number1} e {user_number2} é {multi(user_number1,user_number2)}')
